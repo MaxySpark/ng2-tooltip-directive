121ce07fd907cc9422671b4201ac2bcf258f4779
MaxySpark <me@maxyspark.me>
2020-04-29 14:38:04 +0530

    ok

---
+++ lib/dist/ngx-tooltip-directive.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { TooltipOptionsService as ɵa } from './src/tooltip/tooltip-options.service';
---
+++ lib/dist/public_api.d.ts
@@ -0,0 +1,4 @@
+export * from './src/tooltip/tooltip.component';
+export * from './src/tooltip/tooltip.directive';
+export * from './src/tooltip/tooltip.module';
+export * from './src/tooltip/tooltip-options.interface';
